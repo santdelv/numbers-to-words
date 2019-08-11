d2463dde0de2a8f159dedca9bbdafac6cb02ca27
Santiago Del Valle <santdelv@gmail.com>
2019-08-11 11:54:02 -0500

    Initial refactor to reduce duplicated code

--- src/main/java/com/sonatype/santdelv/takehomeproject/numberstowords/impl/NumberConverterImpl.java
+++ src/main/java/com/sonatype/santdelv/takehomeproject/numberstowords/impl/NumberConverterImpl.java
@@ -5,9 +5,7 @@ import com.sonatype.santdelv.takehomeproject.numberstowords.exceptions.InvalidNu
 import com.sonatype.santdelv.takehomeproject.numberstowords.validators.NumberStringValidator;
 import com.sonatype.santdelv.takehomeproject.numberstowords.validators.impl.NumberStringValidatorImpl;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class NumberConverterImpl implements NumberConverter {
 
@@ -46,12 +44,10 @@ public class NumberConverterImpl implements NumberConverter {
         intializerMap.put(70, "seventy");
         intializerMap.put(80, "eighty");
         intializerMap.put(90, "ninety");
-        intializerMap.put(100, "hundred");
-        intializerMap.put(1000, "thousand");
-        intializerMap.put(1000000, "million");
-        intializerMap.put(1000000000, "billion");
         numberBuildingBlocks = Collections.unmodifiableMap(intializerMap);
     }
+
+    private final static String[] MAGNITUDES = {"hundred", "thousand", "million", "billion"};
 
     public String getNumberAsWords(String number) throws InvalidNumberException {
 
@@ -66,24 +62,28 @@ public class NumberConverterImpl implements NumberConverter {
 
         Integer numberValue = Integer.valueOf(number);
 
-        if (numberValue < 100) {
-            numberAsWords = getNumberLowerThanOneHundred(numberValue);
+        if (numberValue <= 20) {
+            numberAsWords = numberBuildingBlocks.get(Integer.valueOf(numberValue));
         }
 
-        if (numberValue < 1000 && numberValue > 99) {
-            numberAsWords = getNumberLowerThanOneThousand(numberValue, " and ");
-        }
+        if (numberValue > 20){
+            List<String> segments = getThreeCharacterSegments(number);
+            StringBuilder numberSegmentsWords = new StringBuilder();
 
-        if (numberValue < 1000000 && numberValue > 999) {
-            numberAsWords = getNumberLowerThanOneMillion(numberValue);
-        }
+            for (int i = segments.size(); i > 0 ; i--) {
+                // hundreds is a special case because of the and
+                int currentNumber = Integer.valueOf(segments.get(i -1));
+                if(i == 1 && currentNumber > 0) {
+                    numberSegmentsWords.append(getHundredsNumber(currentNumber, " and ", ""));
+                    continue;
+                }
+                if(currentNumber > 0){
+                    numberSegmentsWords.append(getHundredsNumber(Integer.valueOf(segments.get(i - 1)), " ", MAGNITUDES[i-1]));
+                    numberSegmentsWords.append(" ");
+                }
+            }
 
-        if (numberValue < 1000000000 && numberValue > 999999) {
-            numberAsWords = getNumberLowerThanOneBillion(numberValue);
-        }
-
-        if (numberValue >= 1000000000) {
-            numberAsWords = getNumberBiggerThanOneBillion(numberValue);
+            numberAsWords = numberSegmentsWords.toString().trim();
         }
 
         if (isNegativeNumber && !"zero".equals(numberAsWords)){
@@ -92,101 +92,71 @@ public class NumberConverterImpl implements NumberConverter {
         return numberAsWords.substring(0, 1).toUpperCase() + numberAsWords.substring(1);
     }
 
+    private List<String> getThreeCharacterSegments(String number) {
+
+        List<String> segments = new ArrayList<>();
+        String segment;
+
+        if (number.length() <= 3) {
+            segments.add(number);
+            return segments;
+        }
+
+        int segmentSize = number.length() % 3;
+        if(segmentSize == 0){
+            segmentSize = 3;
+        }
+
+        // First segment could be especial so we process it outside of the loop
+        segment = number.substring(0, segmentSize);
+        segments.add(segment);
+        number = number.substring(segmentSize);
+        segmentSize = 3;
+
+        while (number.length() != 0) {
+            segment = number.substring(0, segmentSize);
+            segments.add(segment);
+            number = number.substring(segmentSize);
+        }
+
+        Collections.reverse(segments);
+        return segments;
+    }
+
     private boolean isNegative(String number){
         return number.startsWith("-");
     }
 
-    /**
-     * Given a number lower than hundred.
-     * First we get its lower multiple of 10, get the word that represents it and concatenate with the
-     * remainder of the given number minus the nearest decimal divisor.
-     * @param numberValue number we want to convert.
-     * @return The number representation in english words
-     */
-    private String getNumberLowerThanOneHundred(int numberValue) {
+    private String getHundredsNumber(int numberValue, String separator, String magnitude) {
+        String numberAsWords;
+        String tensPartOfNumber = "";
         if (numberValue <= 20) {
-            return numberBuildingBlocks.get(Integer.valueOf(numberValue));
+            numberAsWords = numberBuildingBlocks.get(Integer.valueOf(numberValue)) + " " + magnitude;
+            return numberAsWords.trim();
         }
 
-        String numberAsWords;
         String basicNumberPart = "";
-        int biggestTenMultiple = (int)Math.floor(numberValue/10) * 10;
-        String tensPartOfNumber = numberBuildingBlocks.get(biggestTenMultiple);
-        int remainder = numberValue - biggestTenMultiple;
-        if(remainder > 0) {
-            basicNumberPart = numberBuildingBlocks.get(numberValue - biggestTenMultiple);
+        int remainder;
+        int biggestTenMultiple = (int)Math.floor((numberValue%100)/10) * 10;
+        if (biggestTenMultiple > 0 && biggestTenMultiple >= 20) {
+            tensPartOfNumber = numberBuildingBlocks.get(biggestTenMultiple) + " ";
+            remainder = numberValue%100 - biggestTenMultiple;
+        }else {
+            remainder = numberValue%100;
         }
-        numberAsWords =  tensPartOfNumber + " " + basicNumberPart;
-        return numberAsWords.trim();
-    }
-
-    /**
-     * Given a number lower than 1000 and bigger than 99 it to words in english.
-     * First we get its hundreds part and then concatenate it with the remainder as words
-     * @param numberValue number we want to convert.
-     * @return The number representation in english words
-     */
-    private String getNumberLowerThanOneThousand(int numberValue, String separator) {
-        //TODO code is too similar to getNumberLowerThanOneHundred, a refactor is needed
-        String numberAsWords;
-        String remainderNumber = "";
+        if(remainder > 0) {
+            basicNumberPart = numberBuildingBlocks.get(remainder);
+        }
+        String notHundredsPartOfNumber = (tensPartOfNumber + basicNumberPart).trim();
+        if (notHundredsPartOfNumber.isEmpty()) {
+            separator = "";
+        }
         String hundredsPartOfNumber = "";
         int biggestHundredNumber = (int)Math.floor(numberValue/100);
         if (biggestHundredNumber > 0) {
-            hundredsPartOfNumber = numberBuildingBlocks.get(biggestHundredNumber) + " " + "hundred";
-        }else {
-            separator = " ";
+            hundredsPartOfNumber = numberBuildingBlocks.get(biggestHundredNumber) + " " + "hundred" + separator;
         }
-        int remainder = numberValue - (biggestHundredNumber * 100);
-        if(remainder > 0) {
-            remainderNumber = separator + getNumberLowerThanOneHundred(remainder);
-        }
-        numberAsWords =  hundredsPartOfNumber + remainderNumber;
-        return numberAsWords.trim();
-    }
-
-    private String getNumberLowerThanOneMillion(int numberValue) {
-        String numberAsWords;
-        String remainderNumber = "";
-        String thousandsPartOfNumber = "";
-        int biggestThousandNumber = (int)Math.floor(numberValue/1000);
-        if(biggestThousandNumber > 0){
-            thousandsPartOfNumber = getNumberLowerThanOneThousand(biggestThousandNumber, " ") + " " + "thousand";
-        }
-        int remainder = numberValue - (biggestThousandNumber * 1000);
-        if(remainder > 0) {
-            remainderNumber = " " + getNumberLowerThanOneThousand(remainder, " and ");
-        }
-        numberAsWords =  thousandsPartOfNumber + remainderNumber;
-        return numberAsWords.trim();
-    }
-
-    private String getNumberLowerThanOneBillion(int numberValue) {
-        String numberAsWords;
-        String remainderNumber = "";
-        String millionsPartOfNumber = "";
-        int biggestMillionNumber = (int)Math.floor(numberValue/1000000);
-        if (biggestMillionNumber > 0){
-            millionsPartOfNumber = getNumberLowerThanOneThousand(biggestMillionNumber, " ") + " " + "million";
-        }
-        int remainder = numberValue - (biggestMillionNumber * 1000000);
-        if(remainder > 0) {
-            remainderNumber = getNumberLowerThanOneMillion(remainder);
-        }
-        numberAsWords =  millionsPartOfNumber + " " + remainderNumber;
-        return numberAsWords.trim();
-    }
-
-    private String getNumberBiggerThanOneBillion(int numberValue) {
-        String numberAsWords;
-        String remainderNumber = "";
-        int biggestBillionNumber = (int)Math.floor(numberValue/1000000000);
-        String billionPartOfNumber = numberBuildingBlocks.get(biggestBillionNumber) + " " + "billion";
-        int remainder = numberValue - (biggestBillionNumber * 1000000000);
-        if(remainder > 0) {
-            remainderNumber = " " + getNumberLowerThanOneBillion(remainder);
-        }
-        numberAsWords =  billionPartOfNumber + remainderNumber;
+        numberAsWords =  (hundredsPartOfNumber + notHundredsPartOfNumber + " ").trim() + " " + magnitude;
         return numberAsWords.trim();
     }
 
